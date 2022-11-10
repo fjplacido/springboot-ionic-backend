2316bee4f51b3c11120e1efa9f7356c0231affb2
Francisco Cunha <fj.placido@gmail.com>
2022-11-10 09:25:48 -0300

    Project created.

---
+++ src/main/java/br/com/jsolutions/SpringbootExampleProjectApplication.java
@@ -0,0 +1,13 @@
+package br.com.jsolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootExampleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootExampleProjectApplication.class, args);
+	}
+
+}
