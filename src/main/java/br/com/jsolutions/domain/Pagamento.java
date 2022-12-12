5eb8a7d720ad2a1294ccd56f1c7d93861a92455e
Francisco Cunha <fj.placido@gmail.com>
2022-12-12 15:13:47 -0300

    Implementação do Endpoint /pedidos/{id}

--- src/main/java/br/com/jsolutions/domain/Pagamento.java
+++ src/main/java/br/com/jsolutions/domain/Pagamento.java
@@ -11,6 +11,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import br.com.jsolutions.domain.enums.EstadoPagamento;
 
 @Entity
@@ -23,6 +25,7 @@ public abstract class Pagamento implements Serializable {
 	private Integer id;
 	private Integer estado;
 
+	@JsonBackReference
 	@OneToOne
 	@JoinColumn(name = "pedido_id")
 	@MapsId
---
+++ src/main/java/br/com/jsolutions/services/PedidoService.java
@@ -0,0 +1,24 @@
+package br.com.jsolutions.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.jsolutions.domain.Pedido;
+import br.com.jsolutions.repositories.PedidoRepository;
+import br.com.jsolutions.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+
+}
