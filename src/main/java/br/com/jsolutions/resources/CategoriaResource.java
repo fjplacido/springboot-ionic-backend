f77b6b7a1fc23502bd14db48b7539c4790a881cd
fcunha <fcunha@ncc17277.tecgraf.puc-rio.br>
2022-12-29 21:47:45 -0300

    Listando todas as Categorias com DTO

--- src/main/java/br/com/jsolutions/resources/CategoriaResource.java
+++ src/main/java/br/com/jsolutions/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package br.com.jsolutions.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.jsolutions.domain.Categoria;
+import br.com.jsolutions.dto.CategoriaDTO;
 import br.com.jsolutions.services.CategoriaService;
 
 @RestController
@@ -48,5 +51,14 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream() //
+				.map(e -> new CategoriaDTO(e)) //
+				.collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
